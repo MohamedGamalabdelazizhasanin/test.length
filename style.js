2ba5e9695d35251bd61ef0fe18a31a14402c0a6c
Mohamed Gamal <150333650+MohamedGamalabdelazizhasanin@users.noreply.github.com>
2024-03-11 04:57:35 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,56 @@
+
+setInterval(() =>{
+ let hh = document.getElementById('hh')
+let mm = document.getElementById('mm')
+let ss = document.getElementById('ss')
+let sec_dot = document.querySelector('.sec_dot')
+let min_dot = document.querySelector('.min_dot')
+let hr_dot = document.querySelector('.hr_dot')
+let hr = document.getElementById('hr')
+let mn = document.getElementById('mn')
+let sc =document.getElementById('sc')
+
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+hh.style.strokeDashoffset = 510 - (510 * h) / 12;
+// hrs clock 
+mm.style.strokeDashoffset = 630 - (630 * m) / 60;
+//minutes
+ss.style.strokeDashoffset = 760 - (760 * s) / 60;
+//minutes
+
+sec_dot.style.transform = `rotateZ(${s * 6}deg)`
+
+min_dot.style.transform = `rotateZ(${m * 6}deg)`
+
+hr_dot.style.transform = `rotateZ(${h * 30}deg)`
+
+
+hr.style.transform =  `rotateZ(${h * 30}deg)`
+mn.style.transform = `rotateZ(${m * 6}deg)`
+sc.style.transform = `rotateZ(${s * 6}deg)`
+
+})
+
+
+
+
+
+// let hh = document.getElementById('hh')
+// let mm = document.getElementById('mm')
+// let ss = document.getElementById('ss')
+
+// let h = new Date().getHours();
+// let m = new Date().getMinutes();
+// let s = new Date().getSeconds();
+
+// hh.style.strokeDashoffset = 510 - (510 * h) / 12;
+// // hrs clock 
+// mm.style.strokeDashoffset = 630 - (630 * m) / 60;
+// //minutes
+// ss.style.strokeDashoffset = 760 - (760 * s) / 60;
+// //minutes
+
